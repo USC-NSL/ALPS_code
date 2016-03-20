3a014c9c4590401caff363eef34b64b09c41fbdf
sugartom <sugartom.hu@gmail.com>
2016-03-19 22:48:56 -0700

    evaluate fov for fire hydrant, find bug in distance-based clustering. Need to fix.

--- test_la/parse_detect_res_from_yolo.py
+++ test_la/parse_detect_res_from_yolo.py
@@ -10,11 +10,15 @@ if __name__ == '__main__':
 
 
 	# ===============================================================================
-	file_name = "basic_image_database_yolo_result.txt"
+	file_name = "fov_analysis_output_formal.txt"
 	# ===============================================================================
 
 	f_input = open(work_dir + file_name, "r").readlines()
 
-	for i in range(len(f_input) / 2):
-		tmp = f_input[i * 2].rstrip()[:-4].split('_')
+	# for i in range(len(f_input) / 2):
+	# 	tmp = f_input[i * 2].rstrip()[:-4].split('_')
+	# 	print "%s,%s" % (tmp[4], tmp[5])
+
+	for i in range(len(f_input)):
+		tmp = f_input[i].rstrip().split('_')
 		print "%s,%s" % (tmp[4], tmp[5])

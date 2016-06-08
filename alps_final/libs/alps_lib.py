b585d887044cbefe49de9c3050b3074c66a732ff
sugartom <sugartom.hu@gmail.com>
2016-06-08 19:41:07 -0400

    create directory alps_final, and re-write standing point generation part

---
+++ alps_final/libs/alps_lib.py
@@ -0,0 +1,23 @@
+import urllib2
+import json
+
+
+
+# opening the required url, and printing corresponding metadata
+# input: url
+# output: json data from url
+def execute_url(url):
+	# replace all space with ASIC II
+	url = url.replace(" ", "%20")
+	print "[alps_lib] executing the following url:"
+	print "%s" % url
+
+	# Send the GET request to the Place details service (using url from above)
+	response = urllib2.urlopen(url)
+
+	# Get the response and use the JSON library to decode the JSON
+	json_raw = response.read()
+	json_data = json.loads(json_raw)
+
+	return json_data
+
